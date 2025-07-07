5b9261cc343d33e6ce0f1c668b06bfb0caba00b5
Vnywvy <bohdan05zbr@gmail.com>
2025-07-07 19:15:23 +0300

    added cv, minor changes

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -215,7 +215,7 @@ const Hero = () => {
               </svg>
               <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white to-transparent opacity-30 transform -translate-x-full group-hover:translate-x-full transition duration-700"></div>
             </button>
-            <Link to="/">
+            <Link to="/CV_Bohdan_Zhytchenko.pdf" target="_blank">
               <button className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 relative group overflow-hidden rounded-full p-2 bg-transparent text-gray-50 hover:bg-transparent hover:text-gray-50 transition-transform duration-500 hover:scale-105 hover:-rotate-12">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
